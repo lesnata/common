e82974420f398169308e436cca813f98f6172e3c
lesnata <lesyk.natalie@gmail.com>
2019-10-25 13:55:08 +0200

    hw5 added pytests

--- tests/tests_practice/pytests.py
+++ tests/tests_practice/pytests.py
@@ -1,4 +1,4 @@
-
+# import mock
 import builtins
 import pytest
 
@@ -9,18 +9,16 @@ from homework import task10_reversed_string, task11_time_converter, task12_large
 
 class TestsHomework:
     def test_task10_reversed_string(self):
-        string = 'I am a good developer. I am also a writer'
+        string = 'Hello World and Coders'
         actual_result = task10_reversed_string(string)
         expected_result = string[::-1]
         assert actual_result == expected_result
 
-    @pytest.mark.parametrize('num, result', [(120, '2:0'), (187, '3:7'), (675, '11:15')])
+    @pytest.mark.parametrize('num, result', [(63, '1:3'), (120, '2:0'), (58, '00:58')])
     def test_task11_time_converter(self, num, result):
         assert task11_time_converter(num) == result
 
-    @pytest.mark.parametrize('string, expected_output', [('fun&!! time', 'time'),
-                                                         ('I love dogs', 'love'),
-                                                         ('I am a #verygood developer', 'developer')])
+    @pytest.mark.parametrize('string, expected_output', [('fun&!! time', 'time'), ('I love dogs', 'love')])
     def test_task12_largest_word(self, string, expected_output):
         assert task12_largest_word(string) == expected_output
 
@@ -28,36 +26,34 @@ class TestsHomework:
         with mock.patch.object(builtins, 'input', lambda _: 'My name is Michele'):
             assert task13_backwards_string() == 'Michele is name My'
 
-    def test_task14_fibonacci_sequence(self):
-        with mock.patch.object(builtins, 'input', lambda _: 7):
-            assert task14_fibonacci_sequence() == '1, 1, 2, 3, 5, 8, 13'
+    @pytest.mark.parametrize('tested_use_input, expected_output', [(7, [1, 1, 2, 3, 5, 8, 13]), (4, [1, 1, 2, 3])])
+    def test_task14_fibonacci_sequence(self, tested_user_input, expected_output):
+        assert task14_fibonacci_sequence(tested_user_input) == expected_output
 
-    @pytest.mark.parametrize('lst, expected_output', [([1, 2, 3, 4, 21, 34, 55, 89], [2, 4, 34]),
-                                                      ([0, 2, -3, -4, -6, 7, -10], [0, 2, -4, -6, -10]),
+    @pytest.mark.parametrize('some_list, expected_output', [([1, 4, 9, 16, 25, 36, 49, 64, 81, 100], [4, 16, 36, 64, 100]),
                                                       ([2, 5, 3, 4, 3], [2, 4])])
-    def test_task15_even_list(self, lst, expected_output):
-        assert task15_even_list(lst) == expected_output
+    def test_task15_even_list(self, some_list, expected_output):
+        assert task15_even_list(some_list) == expected_output
 
     def test_task16_sum_all_numbers(self):
         with mock.patch.object(builtins, 'input', lambda _: 4):
             assert task16_sum_all_numbers() == 10
 
-    @pytest.mark.parametrize('num, result', [(4, 24), (10, 3628800), (24, 620448401733239439360000)])
+    @pytest.mark.parametrize('num, result', [(4, 24), (10, 3628800)])
     def test_task17_factorial(self, num, result):
         assert task17_factorial(num) == result
 
     @pytest.mark.parametrize('string, expected_output', [('abcd', 'bcdE'),
-                                                         ('klmnsaprst', 'lmnOtbqstU'),
-                                                         ('dmtr', 'EnUs')])
+                                                         ('cat', 'dbU'),
+                                                         ('mouse', 'npvtf')])
     def test_task18_alphabet_modification(self, string, expected_output):
         assert task18_alphabet_modification(string) == expected_output
 
     @pytest.mark.parametrize('string, expected_output', [('edcba', 'abcde'),
-                                                         ('!?klmnsaprst%%', 'aklmnprsst'),
-                                                         ('wwwdmtr_', 'dmrtwww')])
+                                                         ('hello', 'ehllo')])
     def test_task19_alphabetical_string(self, string, expected_output):
         assert task19_alphabetical_string(string) == expected_output
 
-    @pytest.mark.parametrize('num1, num2', [(13, 13), (-98, -98), (0, 0)])
+    @pytest.mark.parametrize('num1, num2', [(13, 13), (-1, -1), (0, 0)])
     def task20_numbers_comparison(self, num1, num2):
         assert task20_numbers_comparison(num1, num2) == '-1'
