6d6d8259bd1f550834075efb58e44d72b9c8d5e1
lesnata <lesyk.natalie@gmail.com>
2019-10-29 19:41:58 +0100

    hw7 updated

--- flask/application.py
+++ flask/application.py
@@ -1,21 +1,10 @@
-# import os
-# import requests
-
 from flask import Flask, session, render_template, request, redirect
 
 app = Flask(__name__)
 
-
-# # Check for environment variable
-# if not os.getenv("DATABASE_URL"):
-#     raise RuntimeError("DATABASE_URL is not set")
-#
-# # Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
-
 fruits_list = ['Melon', 'Apple', 'Strawberry', 'Grape']
 vegetables_list = ['Beans', 'Carrot', 'Beetroot', 'Cucumber']
+
 
 @app.route("/")
 def home():
@@ -47,3 +36,4 @@ def page404():
 
 
 app.run(debug=True)
+
